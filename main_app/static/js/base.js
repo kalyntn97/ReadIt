b6c8a4e2a088a9cd105053bfbea0cdcd2227f4df
kalyntn97 <kalyntn@gmail.com>
2023-11-10 03:35:36 -0500

    finish responsive styling

--- main_app/static/js/base.js
+++ main_app/static/js/base.js
@@ -4,16 +4,25 @@ const loginForm = document.querySelector('.login-form')
 const loginBtn = document.getElementById('login-btn')
 const closeBtn = document.getElementById('close-btn')
 
-loginBtn.addEventListener('click', () => {
+loginBtn?.addEventListener('click', () => {
   modalRoot.classList.add('visible')
   // loginForm.classList.add('visible')
 })
 
-closeBtn.addEventListener('click', () => {
+closeBtn?.addEventListener('click', () => {
   modalRoot.classList.remove('visible')
   // loginForm.classList.remove('visible')
 })
 
+const navBar = document.querySelector('.nav')
+const menuBtn = document.querySelector('.menu')
+
+menuBtn.addEventListener('click', () => {
+  console.log('clicked')
+  navBar.classList.toggle('show')
+})
+
+// conflict with login form
 // modalRoot.addEventListener('click', () => {
 //   modalRoot.classList.remove('visible')
 // })
